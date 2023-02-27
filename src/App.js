a9917b85c710036a76673f3326bf8f241a326e6b
RehmanMujtaba <rehmanmujtaba@outlook.com>
2023-02-27 01:19:59 -0500

    A basic React layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function ProgressTrackerWithGoals() {
+  const [progress, setProgress] = useState(0);
+  const [goals, setGoals] = useState([]);
+  const [newGoal, setNewGoal] = useState('');
+  
+  const handleIncrease = () => {
+    setProgress(progress + 10);
+  };
+  
+  const handleDecrease = () => {
+    setProgress(progress - 10);
+  };
+  
+  const handleAddGoal = () => {
+    setGoals([...goals, newGoal]);
+    setNewGoal('');
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ProgressTrackerWithGoals">
+      <h1>Progress Tracker</h1>
+      <p>Current Progress: {progress}%</p>
+      <button onClick={handleIncrease}>Increase Progress</button>
+      <button onClick={handleDecrease}>Decrease Progress</button>
+      <hr />
+      <h2>Goals</h2>
+      <ul>
+        {goals.map((goal, index) => (
+          <li key={index}>{goal}</li>
+        ))}
+      </ul>
+      <input 
+        type="text" 
+        placeholder="Enter a new goal" 
+        value={newGoal} 
+        onChange={(e) => setNewGoal(e.target.value)} 
+      />
+      <button onClick={handleAddGoal}>Add Goal</button>
     </div>
   );
 }
 
-export default App;
+export default ProgressTrackerWithGoals;
